865753d925313c267154b7908e081218def4c544
Salih Ayazdır <salihayazdir@gmail.com>
2023-05-19 19:13:00 +0300

    ef first migration

---
+++ FoodOrder_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using FoodOrder_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodOrder_API.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>().HasData(
+            new Item()
+            {
+                Id = 1,
+                Name = "Item1",
+                Description = "Descr..",
+                Price = 10,
+            },
+            new Item()
+            {
+                Id = 2,
+                Name = "Item2",
+                Description = "Descr..",
+                Price = 20,
+            },
+            new Item()
+            {
+                Id = 3,
+                Name = "Item3",
+                Description = "Descr..",
+                Price = 30,
+            }
+            );
+        }
+    }
+}
---
+++ FoodOrder_API/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodOrder_API.Models
+{
+    public class Item
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
